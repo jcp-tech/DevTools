3892d59f643e2edca4c8460b6c8d2d3a275f8399
jcp-tech <jonathan.chacko@maritimesupports.com>
2025-08-27 19:03:19 -0400

    Remove custom ADK patches and update google-adk version
    Deleted the custom_adk_patches.py file and related references, as custom timeout logic is no longer needed. Updated google-adk to version 1.12.0 in requirements.txt. Commented out OAuth2 authentication and authServices configuration in agent.py and tools.yaml, simplifying toolset initialization and removing custom authentication logic. Updated .gitignore to exclude the custom_utils directory.

--- smooth-ocean/agent.py
+++ smooth-ocean/agent.py
@@ -1,42 +1,42 @@
 # smooth-ocean.tech.ai
 
 from google.adk.agents import Agent, LlmAgent
-from google.adk.tools.mcp_tool.mcp_toolset import (
-    MCPToolset,
-    StdioServerParameters
-)
+# from google.adk.tools.mcp_tool.mcp_toolset import (
+#     MCPToolset,
+#     StdioConnectionParams,
+#     StdioServerParameters
+# )
 from google.adk.auth import AuthCredentialTypes, AuthCredential, OAuth2Auth
 from google.adk.tools.toolbox_toolset import ToolboxToolset # from toolbox_core import ToolboxClient
 from .prompt import DB_MCP_PROMPT, CODE_MCP_PROMPT
 from .tools import get_lookup_url
 from dotenv import load_dotenv
-from pathlib import Path
+# from pathlib import Path
 # import asyncio
 import os
 
 load_dotenv()
 
 MODEL = os.getenv('GOOGLE_GENAI_MODEL', 'gemini-2.0-flash')
+
+# GITHUB_PAT = os.getenv('GITHUB_PERSONAL_ACCESS_TOKEN')
 TOOLSET_LINK = os.getenv('TOOLSET_LINK', 'http://127.0.0.1:5000')
-GOOGLE_CLIENT_ID = os.getenv('GOOGLE_CLIENT_ID')
-GOOGLE_CLIENT_SECRET = os.getenv('GOOGLE_CLIENT_SECRET')
-GITHUB_PAT = os.getenv('GITHUB_PERSONAL_ACCESS_TOKEN')
-
-auth_credential = AuthCredential(
-    auth_type=AuthCredentialTypes.OAUTH2,
-    oauth2=OAuth2Auth(
-        client_id=GOOGLE_CLIENT_ID, 
-        client_secret=GOOGLE_CLIENT_SECRET
-    ),
-)
-
+# GOOGLE_CLIENT_ID = os.getenv('GOOGLE_CLIENT_ID')
+# GOOGLE_CLIENT_SECRET = os.getenv('GOOGLE_CLIENT_SECRET')
+# auth_credential = AuthCredential(
+#     auth_type=AuthCredentialTypes.OAUTH2,
+#     oauth2=OAuth2Auth(
+#         client_id=GOOGLE_CLIENT_ID, 
+#         client_secret=GOOGLE_CLIENT_SECRET
+#     ),
+# )
 db_toolset = ToolboxToolset(
     server_url=TOOLSET_LINK,
     toolset_name="master_toolset", # 'default'
+    # auth_token_getters={
+    #     'my-google-auth': auth_credential,
+    # }
     # tool_names=[],
-    auth_token_getters={
-        'my-google-auth': auth_credential,
-    }
     # bound_params={
     #     'my-mysql': lambda: {
     #         'host': os.getenv('MYSQL_HOST', 'localhost'),
